0ea2299d8ef07b1be8eb98c79ef3bdf608846271
Daniel Alves <64676391+DanielFrontEndDev@users.noreply.github.com>
2023-06-21 10:47:44 -0300

    feat: get ip

--- app.py
+++ app.py
@@ -4,22 +4,18 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/bonusActivation", methods=['POST'])
+@app.route("/getOutboundIP", methods=['GET'])
 def index():
-    if request.method == 'POST':
-        try:
-            data = request.get_json()
-            url_to_post = 'https://sb2bonus-integration-altenar2-stage.biahosted.com/api/Bonus/CreateBonusByDeposit/json'
-            
-            proxyDict = { 
-                  "http"  : os.environ['QUOTAGUARDSTATIC_URL'], 
-                  "https" : os.environ['QUOTAGUARDSTATIC_URL']
-            }
-            
-            response = requests.post(url_to_post, json=data, proxies=proxyDict)
-            return jsonify({"status": "success", "response": response.json()}), 200
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
+    try:
+        proxyDict = { 
+            "http"  : os.environ['QUOTAGUARDSTATIC_URL'], 
+            "https" : os.environ['QUOTAGUARDSTATIC_URL']
+        }
+        
+        response = requests.get('https://api.ipify.org?format=json', proxies=proxyDict)
+        return jsonify({"status": "success", "response": response.json()}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__=='__main__':
     app.run(port=5000, debug=True)
